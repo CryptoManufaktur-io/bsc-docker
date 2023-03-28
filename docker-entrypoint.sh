88ae04db810b0de242a7e034b7aa355b18d3f1b4
Yorick Downe <yorickdowne@gmail.com>
2023-03-28 15:26:12 +0000

    mv the dir after unpack

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,6 +46,7 @@ if [ -f /home/bsc/data/prune-marker ]; then
 else
   if [ ! -f /home/bsc/data/setupdone ]; then
     wget -q -O - "${SNAPSHOT_FILE}" | tar -I lz4 -xvf - -C /home/bsc/data
+    mv /home/bsc/data/server/dataseed/geth /home/bsc/data/geth
     touch /home/bsc/data/setupdone
   fi
 # Word splitting is desired for the command line parameters
