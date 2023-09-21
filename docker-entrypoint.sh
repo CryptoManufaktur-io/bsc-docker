a1e884185c62f48be00f6ed70b7641488d345707
Yorick Downe <yorickdowne@gmail.com>
2023-09-21 10:02:45 +0000

    Adjust to current bsc mainnet.zip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 wget $(curl -s https://api.github.com/repos/bnb-chain/bsc/releases/latest |grep browser_ |grep mainnet |cut -d\" -f4)
-unzip mainnet.zip
+unzip --overwrite mainnet.zip
 rm mainnet.zip
 
 if [ -z "$(ls -A /home/bsc/data)" ]; then
     geth --datadir /home/bsc/data init genesis.json
 fi
-
-dasel delete -f config.toml Node.LogConfig
-dasel delete -f config.toml Node.HTTPHost
-dasel delete -f config.toml Node.HTTPVirtualHosts
 
 # Duplicate binance-supplied static nodes to trusted nodes
 for string in $(dasel -f config.toml -w json 'Node.P2P.StaticNodes' | jq -r .[]); do
