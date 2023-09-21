7cbd1804cbeebc7513fc22ef5c06ae541ca225e7
Yorick Downe <yorickdowne@gmail.com>
2023-09-21 10:08:58 +0000

    Fix unzip command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget $(curl -s https://api.github.com/repos/bnb-chain/bsc/releases/latest |grep browser_ |grep mainnet |cut -d\" -f4)
-unzip --overwrite mainnet.zip
+unzip -o mainnet.zip
 rm mainnet.zip
 
 if [ -z "$(ls -A /home/bsc/data)" ]; then
